77d11f571a90d6b9521b0b4b9a87f2d944edbea2
Pavan Rikhi <pavan.rikhi@gmail.com>
2020-02-14 13:46:33 -0500

    Initialize Project

---
+++ README.md
@@ -0,0 +1,18 @@
+# hledger-stockquotes
+
+Describe your project here.
+
+
+## Build / Run
+
+You can build the project with stack: `stack build`
+
+For development, you can enable fast builds with file-watching,
+documentation-building, & test-running: `stack test --haddock --fast --file-watch`
+
+To build & open the documentation, run `stack haddock --open hledger-stockquotes`
+
+
+## LICENSE
+
+BSD-3
---
+++ tests/Spec.hs
@@ -0,0 +1,34 @@
+import           Hedgehog
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.Hedgehog
+
+import qualified Hedgehog.Gen                  as Gen
+import qualified Hedgehog.Range                as Range
+
+
+main :: IO ()
+main = defaultMain tests
+
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests, properties]
+
+
+unitTests :: TestTree
+unitTests = testGroup "Unit Tests" [testCase "2+2 = 4" testAddition]
+  where
+    testAddition :: Assertion
+    testAddition = (2 + 2) @?= 4
+
+
+properties :: TestTree
+properties = testGroup
+    "Properties"
+    [testProperty "Addition is Communative" testAdditionCommunative]
+  where
+    testAdditionCommunative :: Property
+    testAdditionCommunative = property $ do
+        let genInt = Gen.int $ Range.linear 0 9001
+        (a, b) <- forAll $ (,) <$> genInt <*> genInt
+        (a + b) === (b + a)
